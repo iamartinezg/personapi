0e3b9cf4e4569b8a7ebc726aaa07f36fd467dfd7
iamartinezg <ivanalejandro20021@gmail.com>
2024-10-16 19:50:20 -0500

    Habilitar Swagger y ajustar configuración de URLs
    Se han agregado servicios de Swagger en el archivo `Program.cs` para habilitar la documentación de la API. Esto incluye la adición de `builder.Services.AddEndpointsApiExplorer()` y `builder.Services.AddSwaggerGen()`. Además, se ha configurado el middleware de Swagger para que se habilite solo en el entorno de desarrollo.
    
    Se han realizado ajustes en el archivo `launchSettings.json` para corregir la estructura JSON y asegurar que las URLs de las aplicaciones se mantengan consistentes.
    
    En el archivo `personapi-dotnet.csproj`, se han añadido referencias a los paquetes `Swashbuckle.AspNetCore` y `Swashbuckle.AspNetCore.SwaggerUI` para soportar la funcionalidad de Swagger.

--- personapi/Program.cs
+++ personapi/Program.cs
@@ -15,6 +15,11 @@ builder.Services.AddScoped<IPersonaRepository, PersonaRepository>();
 builder.Services.AddScoped<IProfesionRepository, ProfesionRepository>();
 builder.Services.AddScoped<ITelefonoRepository, TelefonoRepository>();
 
+// Agregar servicios de Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -24,11 +29,20 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-//app.UseHttpsRedirection();
+// Habilitar middleware de Swagger en desarrollo
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    });
+}
+
+// app.UseHttpsRedirection(); // Puedes descomentar esto si usas HTTPS
 app.UseStaticFiles();
 
 app.UseRouting();
